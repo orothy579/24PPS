28950671fff55b3b37dfb2090e03ea4d2e61ee6d
orothy579 <fstky2050@naver.com>
2024-07-18 13:11:39 +0900

    12 updated

--- 012.py
+++ 012.py
@@ -1,19 +1,22 @@
-n = int(input())
-ans = [0] *n
-a = list(map(int, input().split()))
-mystack = []
+def find_nges(N, A):
+    result = [-1] * N  # 결과를 저장할 리스트를 -1로 초기화
+    stack = []  # 스택 초기화
 
-for i in range(n):
-    while mystack and a[mystack[-1]] < a[i]:
-        ans[mystack.pop()] = a[i]
-    mystack.append(i)
-    
-while mystack:
-    ans[mystack.pop()] = -1
+    for i in range(N):
+        # 스택이 비어있지 않고, 현재 원소가 스택의 꼭대기 원소보다 크면
+        while stack and A[stack[-1]] < A[i]:
+            index = stack.pop()  # 스택의 꼭대기 원소를 pop
+            result[index] = A[i]  # 그 인덱스에 대한 오큰수를 현재 원소로 설정
+        stack.append(i)  # 현재 인덱스를 스택에 추가
 
-result = ""
+    return result
 
-for i in range(n):
-    result += str(ans[i]) + " "
+# 입력 받기
+N = int(input().strip())
+A = list(map(int, input().strip().split()))
 
-print(result)
+# 오큰수 구하기
+result = find_nges(N, A)
+
+# 결과 출력
+print(" ".join(map(str, result)))

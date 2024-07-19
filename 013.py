6313e166e93e462256b59644d8a4f0d7eaeb4919
orothy579 <fstky2050@naver.com>
2024-07-19 17:48:30 +0900

    013.py created

---
+++ 013.py
@@ -0,0 +1,14 @@
+from collections import deque
+
+n = int(input())
+queue = deque()
+
+for i in range(1, n+1):
+    queue.append(i)
+
+while len(queue) > 1:
+    queue.popleft()
+    tmp = queue.popleft()
+    queue.append(tmp)
+
+print(queue[0])
